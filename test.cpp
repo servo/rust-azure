86f72bd685c31f89541b7b0536897e24b59e8bd7
Brian Anderson <banderson@mozilla.com>
2012-04-10 21:41:00 -0700

    Test Xlib + cairo without azure

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
+#include <assert.h>
 #include <unistd.h>
 #include <stdio.h>
-#include "xcb/xcb.h"
+#include <X11/Xlib.h>
+//#include "xcb/xcb.h"
 #include "cairo/cairo.h"
+#include "cairo/cairo-xlib.h"
 #include "mozilla/gfx/2D.h"
 
-int main() {
+/*int main() {
   xcb_connection_t *connection = xcb_connect(NULL, NULL);
   const xcb_setup_t *setup = xcb_get_setup(connection);
   xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
@@ -29,4 +32,51 @@ int main() {
   xcb_disconnect(connection);
 
   return 0;
+}
+*/
+
+#define SIZEX 400
+#define SIZEY 400
+
+void paint(cairo_surface_t *cs) {
+    cairo_t *c;
+    c = cairo_create(cs);
+    cairo_rectangle(c, 0.0, 0.0, SIZEX, SIZEY);
+    cairo_set_source_rgb(c, 0.0, 0.0, 0.5);
+    cairo_fill(c);
+}
+
+int main() {
+    Display *dpy;
+    Window rootwin;
+    Window win;
+    XEvent e;
+    int scr;
+    cairo_surface_t *cs;
+
+    dpy = XOpenDisplay(NULL);
+    assert(dpy);
+
+    scr = DefaultScreen(dpy);
+    rootwin = RootWindow(dpy, scr);
+    win = XCreateSimpleWindow(dpy, rootwin, 1, 1, SIZEX, SIZEY, 0,
+			      BlackPixel(dpy, scr), BlackPixel(dpy, scr));
+
+    XStoreName(dpy, win, "test");
+    XSelectInput(dpy, win, ExposureMask | ButtonPressMask);
+    XMapWindow(dpy, win);
+
+    cs = cairo_xlib_surface_create(dpy, win, DefaultVisual(dpy, 0), SIZEX, SIZEY);
+
+    while(1) {
+	XNextEvent(dpy, &e);
+	if (e.type==Expose && e.xexpose.count<1) {
+	    paint(cs);
+	} else if (e.type==ButtonPress) break;
+    }
+
+    cairo_surface_destroy(cs);
+    XCloseDisplay(dpy);
+
+    return 0;
 }
