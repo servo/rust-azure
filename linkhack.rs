df7e8c3e23a1b2ae9ac5b8c6c83d3e801f6dde5c
Brian Anderson <banderson@mozilla.com>
2013-01-11 16:28:48 -0800

    Use in-tree cairo

--- linkhack.rs
+++ linkhack.rs
@@ -1,12 +1,12 @@
 // Some crumminess to make sure we link correctly
 
 #[cfg(target_os = "linux")]
-#[link_args = "-L. -lcairo -lazure -lstdc++ -lskia"]
+#[link_args = "-L. -lazure -lstdc++ -lskia"]
 #[nolink]
 extern mod m { }
 
 #[cfg(target_os = "macos")]
-#[link_args = "-L. -lcairo -lazure -lstdc++ -framework ApplicationServices \
+#[link_args = "-L. -lazure -lstdc++ -framework ApplicationServices \
 			   -lskia -framework IOSurface -lobjc -framework OpenGL \
 			   -framework Foundation -framework QuartzCore"]
 #[nolink]
