5fc77111bbb003fbcd9ba15e4813b31eccf6943c
Brian Anderson <banderson@mozilla.com>
2013-04-03 18:14:37 -0700

    Add license

--- azure-c.h
+++ azure-c.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef MOZILLA_GFX_AZURE_C_H_
 #define MOZILLA_GFX_AZURE_C_H_
 
