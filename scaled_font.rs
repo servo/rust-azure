7d2543dceeb05741637e6070d700ae3a85495010
Josh Matthews <josh@joshmatthews.net>
2013-04-04 11:33:41 -0400

    Update to 0.6

--- scaled_font.rs
+++ scaled_font.rs
@@ -6,19 +6,21 @@ extern mod cairo;
 
 use azure::{AzScaledFontRef, AzFloat};
 use azure::{AZ_NATIVE_FONT_CAIRO_FONT_FACE, AZ_NATIVE_FONT_MAC_FONT_FACE};
+use azure::{AZ_FONT_STYLE_NORMAL, AZ_NATIVE_FONT_SKIA_FONT_FACE};
 use azure::{struct__AzNativeFont};
 
 use azure_hl::{BackendType, CairoBackend, CoreGraphicsAcceleratedBackend};
 use azure_hl::{CoreGraphicsBackend, Direct2DBackend, NoBackend, RecordingBackend};
 use azure_hl::{SkiaBackend};
 use azure::bindgen::{AzCreateScaledFontForNativeFont, AzReleaseScaledFont};
+use azure::bindgen::{AzCreateFontOptions, AzDestroyFontOptions};
 use cairo::cairo::{cairo_font_face_t, cairo_matrix_t, cairo_scaled_font_t};
 use cairo::cairo::{struct__cairo_matrix};
 use cairo::cairo::bindgen::{cairo_font_face_destroy, cairo_font_options_create};
 use cairo::cairo::bindgen::{cairo_font_options_destroy, cairo_matrix_init_identity, cairo_matrix_scale};
 use cairo::cairo::bindgen::{cairo_scaled_font_create};
 
-use core::libc::{c_void, c_double};
+use core::libc::{c_void, c_double, c_int};
 
 #[cfg(target_os="macos")]
 priv use scaled_font::macos::*;
