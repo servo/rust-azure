f5fe521c850a02ae1f352bfb652e263c0c97d624
Brian Anderson <banderson@mozilla.com>
2013-04-30 14:42:17 -0700

    Disable error that forbids builds with gcc 4.2

--- include/mozilla/Compiler.h
+++ include/mozilla/Compiler.h
@@ -17,7 +17,8 @@
      ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) \
       >= ((major) * 10000 + (minor) * 100 + (patchlevel)))
 #if !MOZ_GCC_VERSION_AT_LEAST(4, 4, 0)
-#  error "mfbt (and Gecko) require at least gcc 4.4 to build."
+// RBA: Not true for servo!
+//#  error "mfbt (and Gecko) require at least gcc 4.4 to build."
 #endif
 #endif
 
