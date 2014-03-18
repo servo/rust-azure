8c1cbd6817241e7f13a985a79a904f1cfac7899e
Lars Bergstrom <lars@lars.com>
2014-03-17 20:10:17 -0500

    Update link libraries to ensure that check-servo-net can link skia

--- linkhack.rs
+++ linkhack.rs
@@ -8,6 +8,8 @@
 #[link(name = "azure")]
 #[link(name = "stdc++")]
 #[link(name = "skia")]
+// skia must come before freetype for linking to succeed
+#[link(name = "freetype")]
 // fontconfig must come before expat for linking to succeed
 #[link(name = "fontconfig")]
 #[link(name = "expat")]
