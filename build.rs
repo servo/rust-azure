30e954281095654d61caf8015c70bc66f00cd85b
Vladimir Vukicevic <vladimir@pobox.com>
2016-06-03 16:34:30 -0400

    Convert to CMake, support MSVC builds

--- build.rs
+++ build.rs
@@ -2,19 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::process::Command;
+extern crate cmake;
+//use std::process::Command;
 use std::env;
 
-
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    assert!(Command::new("make")
-        .args(&["-R", "-f", "makefile.cargo", &format!("-j{}", env::var("NUM_JOBS").unwrap())])
-        .status()
-        .unwrap()
-        .success());
-    println!("cargo:rustc-flags=-L native={}", env::var("OUT_DIR").unwrap());
+    let dst = cmake::Config::new(".").generator("Ninja")
+        .define("CMAKE_C_COMPILER", "cl.exe")
+        .define("CMAKE_CXX_COMPILER", "cl.exe")
+        .define("CMAKE_LINKER", "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\link.exe")
+        .build();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
+    let target = env::var("TARGET").unwrap();
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=static=azure");
         println!("cargo:rustc-link-lib=stdc++");
